e9784ed859fa14fa858f2ab02cccdce9f4d936b2
Konstantin <konstantin.v.modin@gmail.com>
2019-02-27 06:54:14 +0100

    updates

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,20 +3,45 @@ import Spinner from '../spinner';
 import ErrorBoundry from '../error-boundry';
 import './app.css';
 import ErrorButton from '../error-button';
-import { withBookstoreService } from '../hoc';
+import { Route } from 'react-router-dom';
 
-const App = ({ bookstoreService }) => {   
-    console.log(bookstoreService.getBooks());
+
+const App = () => {   
+    
     return (
-        <div className="app">
+        <div classNameName="app">
             <ErrorBoundry>
                     
-                        <h1>Whats app.js!!!</h1>
-                        <Spinner name="John"/>
-                        <ErrorButton />
-                    
+                <h1>Whats app.js!!!</h1>
+                <Spinner name="John"/>
+                <ErrorButton />
+                <div>
+                    <nav className="navbar navbar-expand-lg navbar-dark bg-primary"> 
+                        <div className="collapse navbar-collapse" id="navbarColor01">
+                            <ul className="navbar-nav mr-auto">
+                                <li className="nav-item active">
+                                    <a className="nav-link" href="/">Home</a>
+                                </li>
+                                <li className="nav-item active">
+                                    <a className="nav-link" href="/blog">Blog</a>
+                                </li>
+                                <li className="nav-item active">
+                                    <a className="nav-link" href="/about">About</a>
+                                </li>
+                                <li className="nav-item active">
+                                    <a className="nav-link" href="/shop">Shop</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+
+
+                <Route path="/blog" render={ () => <h1>Blog</h1>}/>
+                <Route path="/about" render={ () => <h1>About</h1>}/>
+                <Route path="/shop" render={ () => <h1>Shop</h1>}/>    
             </ErrorBoundry>
         </div>
     );    
 };
-export default withBookstoreService(App); 
+export default App; 
