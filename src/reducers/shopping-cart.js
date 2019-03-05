61ce0b4ac7418c2e4e24e4b139d714620c4c7951
Konstantin <konstantin.v.modin@gmail.com>
2019-03-05 09:53:49 +0100

    updates

---
+++ src/reducers/shopping-cart.js
@@ -0,0 +1,75 @@
+
+const updateCartItems = (cartItems, idx, item) => {
+    if (item.count === 0) {
+        return [
+            ...cartItems.slice(0, idx),          
+            ...cartItems.slice(idx + 1)
+        ];
+    }    
+    
+    if (idx === -1) {
+        return [
+            ...cartItems,
+            item
+        ];
+    }
+
+    return [
+        ...cartItems.slice(0, idx),
+        item,
+        ...cartItems.slice(idx + 1)
+    ];
+};
+
+const updateCartItem = (book, item = {}, q = 1) => {
+    const { id = book.id, 
+        count = 0, 
+        title = book.title } = item; 
+
+    return {
+        id,
+        title,
+        count: count + q,
+        total: book.price * (count + q)
+    };
+};
+
+const updateOrder = (state, bookId, quantity) => {
+    const { bookList: { books }, shoppingCart: { cartItems}  } = state;
+    
+    const book = books.find(({ id }) => id === bookId);
+    const itemIndex = cartItems.findIndex(({ id })=> id === bookId);
+    const item = cartItems[itemIndex];
+    const newItem = updateCartItem(book, item, quantity);
+    
+    return {           
+        cartItems: updateCartItems(cartItems, itemIndex, newItem),
+        orderTotal: 0                
+    };
+}
+
+const updateShoppingCart = (state, action) => {
+    if (state === undefined) {
+       return {
+            cartItems: [],
+            orderTotal: 0
+        };
+    }
+
+    switch (action.type) {
+        case 'BOOK_ADDED_TO_CART':
+            return updateOrder(state, action.payload, 1);
+        
+        case 'BOOK_DECREASED':
+            return updateOrder(state, action.payload, -1);
+
+        case 'BOOK_DELETED':                   
+            const item = state.shoppingCart.cartItems.find(({ id }) => id === action.payload);
+            return updateOrder(state, action.payload, -item.count);      
+        
+        default: 
+            return state.shoppingCart;
+    }
+};
+
+export default updateShoppingCart;
