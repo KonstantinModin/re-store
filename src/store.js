675fa17ea4622590595d368e88418ab5e0fa903c
Konstantin <konstantin.v.modin@gmail.com>
2019-03-06 06:40:14 +0100

    updates

--- src/store.js
+++ src/store.js
@@ -1,6 +1,29 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const logMiddleware = (store) => (next) => (action) => {
+    console.log(action.type, store.getState());
+    return next(action);
+};
+
+const stringMiddleware = () => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        });
+    }
+    return next(action);
+};
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware, stringMiddleware, logMiddleware));
+
+const myAction = (dispatch) => {
+    setTimeout(() => dispatch({
+        type: 'DELAYED_ACTION'
+    }), 2000);
+};
+
+store.dispatch(myAction);
 
 export default store;
